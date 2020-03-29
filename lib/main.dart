fa72037aba03644bf8da0b9a2d15fd7d6e0dbd52
Jailson Jarvis <startupdobem@gmail.com>
2020-03-29 14:24:38 -0300

    Primeiro Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,49 @@ import 'package:flutter/material.dart';
 
 main() => runApp(QuestionsApp());
 
-class QuestionsApp extends StatelessWidget {
+class QuestionsApp extends StatefulWidget {
+  QuestionAppState createState() {
+    return QuestionAppState();
+  }
+}
+
+class QuestionAppState extends State<QuestionsApp> {
+  var questionSelected = 0;
+  void reply() {
+    setState(() {
+      questionSelected++;
+    });
+  }
+
   @override
   Widget build(BuildContext contex) {
+    final questions = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?'
+    ];
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text('Questions'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Questions'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionSelected]),
+            RaisedButton(
+              child: Text('Respopsta 1'),
+              onPressed: reply,
+            ),
+            RaisedButton(
+              child: Text('Respopsta 2'),
+              onPressed: reply,
+            ),
+            RaisedButton(
+              child: Text('Respopsta 3'),
+              onPressed: reply,
+            )
+          ],
+        ),
       ),
-      body: Text('teste'),
-    ),
     );
   }
 }
